cf1da2d4fd3c042be374babb6a4afbd8f8823cb2
illmattep <kippermint6@gmail.com>
2024-01-21 22:54:57 +0100

    Header component created

---
+++ Server/components/AppHeader.vue
@@ -0,0 +1,7 @@
+<template>
+    <header class="bg-stone-700">
+        <div class="container px-4 py-6">
+            <h1 class="text-white text-2xl font-bold">My App</h1>
+        </div>
+    </header>
+</template>
